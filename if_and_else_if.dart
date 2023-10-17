a13b8b33ec0cc2fb74b08a39c9e3891dfc17208f
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-17 00:26:44 -0700

    if_and_else_if.dart
    enter your marks the result show your rank or grade simple practice

---
+++ if_and_else_if.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+   var myMarks = 949;
+  
+  if(myMarks >= 1050 && myMarks < 1100){
+    print ("Your are the topper student in Kohat board.");
+  }
+  else if(myMarks >= 1000 && myMarks < 1050){
+    print ("Your are the top 100 in your board and collage.");
+  }
+  else if(myMarks >= 900 && myMarks < 1000){
+    print ("Your Grade is A1 OR A+");
+  }
+  else if(myMarks >= 800 && myMarks < 900){
+    print ("Your Grade is B");
+  }
+  else if(myMarks >= 700 && myMarks < 800){
+    print ("Your Grade is C");
+  }
+   else if(myMarks >= 600 && myMarks < 700){
+    print ("Your Grade is D");
+  }
+   else if(myMarks < 600){
+    print ("Your are failed please focus on study");
+  }
+  else {
+    print ("your not student bcz this not marks you are failed.");
+  }
+}
